9683b9eff9c12ad66cb8cddaa4a1002709469406
Chris <73894631+cdepaul06@users.noreply.github.com>
2023-10-18 17:53:13 -0400

    done.

--- functions/script.js
+++ functions/script.js
@@ -1,25 +1,42 @@
-// Get the modal
-var modal = document.getElementById("pdfModal");
+// Get the modals
+var pdfModal = document.getElementById("pdfModal");
+var contactModal = document.getElementById("contactModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("btnShowPdf");
+// Get the button that opens the PDF modal
+var btnPdf = document.getElementById("btnShowPdf");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// PDF button click event
+if (btnPdf) {
+  // Check if btnPdf exists on the page
+  btnPdf.onclick = function () {
+    pdfModal.style.display = "block";
+  };
+}
 
-// When the user clicks the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
+// Contact link click event
+var btnContact = document.querySelector(".navigation a[href='contact.html']");
+if (btnContact) {
+  btnContact.onclick = function (e) {
+    e.preventDefault(); // Prevent navigation
+    contactModal.style.display = "block";
+  };
+}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
+// Close button click events
+var spanCloseButtons = document.getElementsByClassName("close");
+for (var i = 0; i < spanCloseButtons.length; i++) {
+  spanCloseButtons[i].onclick = function () {
+    if (pdfModal) pdfModal.style.display = "none";
+    if (contactModal) contactModal.style.display = "none";
+  };
+}
 
-// When the user clicks anywhere outside of the modal, close it
+// Clicking outside modal events
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == pdfModal) {
+    pdfModal.style.display = "none";
+  }
+  if (event.target == contactModal) {
+    contactModal.style.display = "none";
   }
 };
